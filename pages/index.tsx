fe47365f4542b4f0990d532b66e15e5a2654d565
Kevin Sims <kevinsims@Not-Yours.attlocal.net>
2023-09-08 14:03:48 -0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
+// @ts-ignore
 import { NFTCard } from "./components/nftCard.tsx"
 
 const Home: React.FC = () => {
